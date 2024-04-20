f6d2367b7fdf0fbca82e636e2d7ec95198df946b
ritika <shrestharitika998@gmail.com>
2024-04-20 13:31:40 +0545

    latest week1 Uploaded

---
+++ WEEK/week1.java
@@ -0,0 +1,44 @@
+public class week1 {
+  
+        public static void main (String args[]){
+        
+    /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java.*/
+            System.out.println("HelloWorld!");
+    /*2. Extend/Modify the above program to print two additional lines:
+    
+    / This is my first program
+    / I am on module ST4003CEM
+    
+    / System.out.print (not println) and produces the same output.*/
+    
+            System.out.print("This is my first program\n");
+            
+            System.out.print("I am on module ST4003CEM\n");
+
+            System.out.print("just");
+
+    /* 3. Write a program that prints the following output:
+
+     A "quoted" String is
+            
+    'much' better if you learn
+            
+     the rules of "escape sequences."
+            
+      Also, "" represents an empty
+            
+      String. Don't forget: use \"
+            
+      instead of " !
+            
+      '' is not the same as " */
+        // NOTE: \ is used before "    
+           System.out.println("A \"quoted\" String is");
+           System.out.println("'much' better if you learn");
+           System.out.println("the rules of \"escape sequences.\"");
+           System.out.println("Also,\"\" represents an empty");
+           System.out.println("String.Don't forget:use \\  \"  ");
+           System.out.println("instead of \" ! ");
+           System.out.println("\"is not the same as\"");
+}            
+        }
